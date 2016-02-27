c46b1d254cc1a95a16226ee8bfee747c9a97727c
Lívio Amorim <livioamorim@gmail.com>
2016-02-27 13:28:04 -0300

    Criando Filtros. Epi9 - Até Filtro 1.

---
+++ js/filters/nameFilter.js
@@ -0,0 +1,11 @@
+angular.module("listaTelefonica").filter("name",function () {
+  return function (input) {
+     var listaDeNomes = input.split(" ");
+     var listaDeNomesFormatada = listaDeNomes.map(function (nome) {
+       if (/(da|de|do|dos)/.test(nome)) return nome;
+       return nome.charAt(0).toUpperCase() + nome.substring(1).toLowerCase();
+     });
+     console.log(input);
+     return listaDeNomesFormatada.join(" ");
+  };
+});
