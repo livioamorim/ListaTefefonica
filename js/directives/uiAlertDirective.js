92128ab65fe8b95fc4945b66e3b763feca9686fd
Lívio Amorim <livioamorim@gmail.com>
2016-03-08 17:20:38 -0300

    Episódio 10 - Criando diretivas - Parte 1.

--- js/directives/uiAlertDirective.js
+++ js/directives/uiAlertDirective.js
@@ -1,5 +1,11 @@
 angular.module("listaTelefonica").directive("uiAlert",function () {
   return{
-    templateUrl: "view/alert.html"
+    templateUrl: "view/alert.html",
+    replace: true,
+    restrict: "AE",
+    scope: {
+      title: "@",
+    },
+    transclude: true,
   };
 });
